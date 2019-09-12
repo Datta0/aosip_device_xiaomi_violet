1b2f1dffaf71fa4e98a882ddc06f3522a65e8d0a
datta0 <venkatadattasainimmaturi@gmail.com>
2019-09-12 20:43:31 +0530

    Pixys-made

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosip.mk
+    $(LOCAL_DIR)/pixys_violet.mk
--- pixys_violet.mk
+++ pixys_violet.mk
@@ -13,10 +13,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
 # Inherit some common Aosip stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosip_violet
+PRODUCT_NAME := pixys_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
